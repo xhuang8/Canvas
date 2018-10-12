37aa8e549e98a485bbd65d9964aee44404451ad3
xhuang8 <xhuang8@mail.sfsu.edu>
2018-10-11 17:21:45 -0700

    use a pan gesture to drag faces from the image tray to the canvas

--- Canvas/ViewController/CanvasViewController.swift
+++ Canvas/ViewController/CanvasViewController.swift
@@ -14,6 +14,10 @@ class CanvasViewController: UIViewController {
     @IBOutlet weak var trayView: UIView!
     
     var trayOriginalCenter: CGPoint!
+    
+    var newlyCreatedFace: UIImageView!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
+    
     var trayDownOffset: CGFloat!
     var trayUp: CGPoint!
     var trayDown: CGPoint!
@@ -22,8 +26,9 @@ class CanvasViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        trayDownOffset = 350
+    
+        
+        trayDownOffset = 300
         trayUp = trayView.center // The initial position of the tray
         trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset) // The position of the tray transposed down
 
@@ -31,33 +36,78 @@ class CanvasViewController: UIViewController {
     }
     
     
+    // User can use a pan gesture to move the position of the tray
     @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
-        let location = sender.location(in: view)
+       // let location = sender.location(in: view)
         let velocity = sender.velocity(in: view)
         let translation = sender.translation(in: view)
         
         if sender.state == .began {
             print("Gesture began")
             trayOriginalCenter = trayView.center
-        } else if sender.state == .changed {
+        }
+        else if sender.state == .changed
+        {
             print("Gesture is changing")
             trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
-        } else if sender.state == .ended {
-            if velocity.y > 0{
-                UIView.animate(withDuration: 0.3){
-                    self.trayView.center = self.trayDown
-                }
+        }
+        else if sender.state == .ended
+        {
+            if velocity.y > 0
+            {
+                //spring animation
+                UIView.animate(withDuration:0.8, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.trayView.center = self.trayDown
+                }, completion: nil)
             }
-            else{
-                UIView.animate(withDuration: 0.3)
-                {
-                    self.trayView.center = self.trayUp
-                }
+            else
+            {
+                UIView.animate(withDuration:0.8, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.trayView.center = self.trayUp
+                }, completion: nil)
             }
-            print("Gesture ended")
+           //print("Gesture ended")
         }
     }
     
+    
+    @IBAction func didPanFace(_ sender: UIPanGestureRecognizer) {
+       // let location = sender.location(in: view)
+        //let velocity = sender.velocity(in: view)
+        let translation = sender.translation(in: view)
+        
+        
+        if sender.state == .began{
+            print("Gesture began")
+            let imageView = sender.view as! UIImageView
+            
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            
+            view.addSubview(newlyCreatedFace)
+            
+            newlyCreatedFace.center = imageView.center
+            
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+            
+        }
+        else if sender.state == .changed{
+           print("Gesture is changing")
+           newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+        }
+        else if sender.state == .ended{
+           print("Gesture ended")
+           UIView.animate(withDuration:0.8, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                           animations: { () -> Void in
+                            self.newlyCreatedFace.transform = CGAffineTransform(scaleX: 1, y: 1)
+            }, completion: nil)
+        }
+    }
+    
+
     
 }
 
